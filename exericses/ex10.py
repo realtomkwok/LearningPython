f6b347e801b9d1f2ba1413f58dc807916958986e
Tom Kwok <junhaotom@gmail.com>
2019-02-27 18:39:42 +0800

    Create ex10.py

---
+++ exericses/ex10.py
@@ -0,0 +1,41 @@
+tabbyCat = "\ti'm tabbed in."
+persianCat = "im split \non a line."
+backslachCat = "im \ a \ cat."
+
+fat_cat = """
+i'll do a list:
+\t* cat food
+\t* fishies
+\t* catnip\n\t* grass
+    """
+
+print(tabbyCat)
+print(persianCat)
+print(backslachCat)
+print(fat_cat)
+
+blacklash = "backslash\\"
+singleQuote = "\'single quote\'"    
+doubleQuote = "\"double quote\""
+bell = "\a"
+backspace = "\b"
+formfeed = "\f"
+linefeed = "\n"
+carriageReturn = "\r"
+horizontalTab = "\t hey"
+sixteenBitHexValue = "\u0f1d01"
+verticalTab = "\v deadass?"
+octalValue = "\000"
+
+print(blacklash)
+print(singleQuote)
+print(doubleQuote)
+print(bell)
+print(backspace)
+print(formfeed)
+print(linefeed)
+print(carriageReturn)
+print(horizontalTab)
+print(sixteenBitHexValue)
+print(verticalTab)
+print(octalValue)
