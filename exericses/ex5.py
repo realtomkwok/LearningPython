5c1b0112c1161d25652677bfeee229e68cbc5fb9
Tom Kwok <junhaotom@gmail.com>
2019-02-27 18:40:47 +0800

    commit

---
+++ exericses/ex5.py
@@ -0,0 +1,17 @@
+myName = 'Tom Kwok'
+myAge = 21
+myHeight = 69
+myHeightInCm = myHeight * 2.54
+myWeight = 120
+myWeightInKilo = myWeight * 0.45
+myEyes = 'Black'
+myTeeth = 'White'
+myHair = 'Black'
+
+print(f"Let's talk bout {myName}")
+print(f"He's {myHeight} inches tall, which is {myHeightInCm} centimeters.")
+print(f"He's {myWeight} pounds heavy, which is {myWeightInKilo} kilograms.")
+
+total = myAge + myHeight + myWeight
+print(total)
+
---
+++ exericses/ex9.py
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("here ar ethe days: ", days)
+print("here are the months: ", months)
+
+print("""
+there's something going on here,
+with the three double-quotes.
+we'll be able to type as much as we like
+even 4 lines if we want, or 5, or 6.
+""")
